dad6f24b1a86e8522e9471590480331dfb5d78a1
Wojciech Haberek <w.haberek@scopegroup.com>
2020-05-03 15:45:27 +0200

    Scroll to top page

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,6 +142,8 @@
     }
     
     /* Common */
+    
+    // Smooth scroll to section
     $('a[href*="#"]').on('click', function(e) {
         e.preventDefault()
         
@@ -153,6 +155,30 @@
           'linear'
         )
     })
+    
+    // Scroll to top
+    var scrollTop = $("#scrollTop");
+    
+    $(window).scroll(function() {
+        // declare variable
+        var topPos = $(this).scrollTop();
+        
+        // if user scrolls down - show scroll to top button
+        if (topPos > 100) {
+            $(scrollTop).css("opacity", "1");
+            
+        } else {
+            $(scrollTop).css("opacity", "0");
+        }
+        
+    });
+    $(scrollTop).click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+        
+    });
     /* /Common */
     
     /* Homepage */
